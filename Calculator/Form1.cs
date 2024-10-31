122d5a2d077f9ae0df0cc75af745d2a57eba75c6
KsushaTarasova <ksutaras543@mail.ru>
2024-10-31 12:12:40 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        string operation = "";
+        Font drawFont = new Font("Arial", 16);
+        SolidBrush brush = new SolidBrush(Color.Pink);
+        RadioButton radioButton;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton = (RadioButton)sender;
+            if (radioButton.Checked)
+            {
+                operation = "+";
+            }
+            
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton = (RadioButton)sender;
+            if (radioButton.Checked)
+            {
+                operation = "-";
+            }
+            
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton = (RadioButton)sender;
+            if (radioButton.Checked)
+            {
+                operation = "/";
+            }
+            
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton = (RadioButton)sender;
+            if (radioButton.Checked)
+            {
+                operation = "*";
+            }
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text),
+                b = Convert.ToDouble(textBox2.Text);
+            double result = 0;
+
+            switch(operation) { 
+                case "+":
+                    {
+                        result = a + b;
+                        break;
+                    }
+                    case "-":
+                    {
+                        result = a - b;
+                        break;
+                    }
+                    case "/":
+                    {
+                        result = a / b;
+                        break;
+                    }
+                case "*":
+                    {
+                        result = a * b;
+                        break;
+                    }
+
+            }
+            
+            g = CreateGraphics();
+            Refresh();
+            g.DrawString(result.ToString(), drawFont, brush, 150, 300);
+        }
+    }
+}
